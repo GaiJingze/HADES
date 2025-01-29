957ab9c1369bb19c321fde276a1b00d06a7b43db
GaiJingze <1725802722@qq.com>
2025-01-29 18:02:31 +0800

    aaa

--- black_box/generate_caption.py
+++ black_box/generate_caption.py
@@ -80,6 +80,8 @@ if __name__ == '__main__':
     new_dataset = []
     for line in tqdm(dataset): 
         print(line)
+        print(line["instruction_list"])
+        
         print(f'Generating caption for keyword {line["keyword"]}')
 
         start_time = time.time()
