733bb4bd353bbd34527a0d624d6da88af051ac54
GaiJingze <1725802722@qq.com>
2025-01-30 05:57:00 +0800

    Update evaluate_for_black_box.py

--- eval/evaluate/evaluate_for_black_box.py
+++ eval/evaluate/evaluate_for_black_box.py
@@ -170,8 +170,10 @@ def main() -> None:
             create_step_excel(scores, excel_path)
             
         print(result_dict)
-
+        
         results_path = eval_dataset_path.replace('gen_results', 'eval_results') + '/' + 'scores.xlsx'
+        if not os.path.exists(results_path):
+            os.makedirs(results_path)
         create_excel(result_dict, filename=results_path)
 
     time_cost = time.time() - start_time
