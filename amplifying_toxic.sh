2592112c418e82c5ba9c9b81f9589bc2bd8d3f59
GaiJingze <1725802722@qq.com>
2025-01-29 16:27:38 +0800

    change working directory

--- amplifying_toxic.sh
+++ amplifying_toxic.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 export OPENAI_API_KEY="your_openai_api_key_here" # Set OpenAI API key as an environment variable
-TEXT_DIR="./dataset/instructions" # Assuming this is the directory where instructional texts are stored
-OUTPUT_DIR="./dataset/black_box/captions" # Assuming this is the directory where generated captions will be saved
-SD_MODEL_PATH="./checkpoint/pixart" # Path to PixArtAlphaPipeline pretrained model
-LLAVA_MODEL_PATH="./checkpoint/llava-1.5-7b" # Path to LLaMA pretrained model
-SD_IMAGE_DIR_BASE="./dataset/black_box/images" # Base directory to save SD images
-LOG_DIR_BASE="./dataset/black_box/logs" # Base directory to save logs
+TEXT_DIR="../dataset/instructions" # Assuming this is the directory where instructional texts are stored
+OUTPUT_DIR="../dataset/black_box/captions" # Assuming this is the directory where generated captions will be saved
+SD_MODEL_PATH="../checkpoint/pixart" # Path to PixArtAlphaPipeline pretrained model
+LLAVA_MODEL_PATH="../checkpoint/llava-1.5-7b" # Path to LLaMA pretrained model
+SD_IMAGE_DIR_BASE="../dataset/black_box/images" # Base directory to save SD images
+LOG_DIR_BASE="../dataset/black_box/logs" # Base directory to save logs
 
 # Array of scenarios
 SCENARIOS=('Animal' 'Self-harm' 'Privacy' 'Violence' 'Financial')
@@ -15,19 +15,19 @@ for SCENARIO in "${SCENARIOS[@]}"; do
   echo "Processing scenario: $SCENARIO"
 
   # Generate captions
-  PYTHONPATH=./ python black_box/generate_caption.py \
+  python black_box/generate_caption.py \
     --text-dir "$TEXT_DIR" \
     --output-dir "$OUTPUT_DIR" \
     --scenario "$SCENARIO"
     --wor
   
   # Generate sd_captions
-  PYTHONPATH=./ python black_box/generate_sd_caption.py \
+  python black_box/generate_sd_caption.py \
     --text-dir "$OUTPUT_DIR" \
     --scenario "$SCENARIO"
   
   # Optimize harm
-  PYTHONPATH=./ python black_box/optimize_harm.py \
+  python black_box/optimize_harm.py \
     --sd_model_path "$SD_MODEL_PATH" \
     --llava_model_path "$LLAVA_MODEL_PATH" \
     --input_file "$OUTPUT_DIR" \
